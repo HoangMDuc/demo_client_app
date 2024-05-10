abc59f8550170026ee9b6a2ee719c2a1a5be07f8
HMD <hoangduc1882002@gmail.com>
2024-05-10 21:25:36 +0700

    update

--- app/src/main/aidl/com/example/openssldemo/IMyAidlInterface.aidl
+++ app/src/main/aidl/com/example/openssldemo/IMyAidlInterface.aidl
@@ -8,7 +8,11 @@ interface IMyAidlInterface {
      * Demonstrates some basic types that you can use as parameters
      * and return values in AIDL.
      */
-
-    void register(String packageID);
-
+void register(String packageID);
+    void store(String packageID, String dataValue, String dataType);
+    String load(String packageID, String dataType );
+    // these function is for testing connection
+    int test(int x);
+    String noti();
+    int getColor();
 }
--- app/src/main/java/com/example/clientapp/MainActivity.kt
+++ app/src/main/java/com/example/clientapp/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.clientapp
 
+import android.R
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
+import android.os.RemoteException
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.clientapp.databinding.ActivityMainBinding
 import com.example.openssldemo.IMyAidlInterface
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.security.KeyStoreException
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "MainActivity - Client App"
         const val DATA_VAULT_PACKAGE = "com.example.openssldemo"
         const val SERVICE_NAME = "MainService"
-        const val REGISTER_ACTION = "com.example.openssldemo.REGISTER"
-        const val STORAGE_ACTION = "com.example.openssldemo.STORAGE"
-        const val LOAD_ACTION = "com.example.openssldemo.LOAD"
         const val PACKAGE_ID = "packageID"
 
     }
@@ -29,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     private val mConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             isBound = true
-            mService = IMyAidlInterface.Stub.asInterface(service)
+            mService = IMyAidlInterface.Stub.asInterface(service as IBinder)
             Toast.makeText(applicationContext, "Connected to remote service", Toast.LENGTH_SHORT)
                 .show()
         }
@@ -48,8 +51,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-
         setContentView(binding.root)
+
+        val listType: List<String> = listOf("password", "username", "email")
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, listType)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.spinnerType.adapter = adapter
+
         binding.apply {
             registerButton.setOnClickListener { handleRegister() }
             storeButton.setOnClickListener { handleStore() }
@@ -59,27 +67,62 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+//        Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+//        val intent = Intent()
+//        //intent.setAction(REGISTER_ACTION)
+//        intent.setComponent(ComponentName(DATA_VAULT_PACKAGE, "$DATA_VAULT_PACKAGE.$SERVICE_NAME"))
+//        //intent.putExtra(PACKAGE_ID, packageName)
+//        bindService(intent, mConnection, BIND_AUTO_CREATE)
         Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, isBound.toString(), Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this,( mService == null).toString(), Toast.LENGTH_SHORT).show()
         val intent = Intent()
         //intent.setAction(REGISTER_ACTION)
         intent.setComponent(ComponentName(DATA_VAULT_PACKAGE, "$DATA_VAULT_PACKAGE.$SERVICE_NAME"))
-        //intent.putExtra(PACKAGE_ID, packageName)
+        intent.putExtra(PACKAGE_ID, packageName)
         bindService(intent, mConnection, BIND_AUTO_CREATE)
     }
 
     private fun handleRegister() {
-//        Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
-//        val intent = Intent()
-//        intent.setAction(REGISTER_ACTION)
-//        intent.setComponent(ComponentName(DATA_VAULT_PACKAGE, "$DATA_VAULT_PACKAGE.$SERVICE_NAME"))
-//        intent.putExtra(PACKAGE_ID, packageName)
-//        bindService(intent, mConnection, BIND_AUTO_CREATE)
-        if(isBound) {
-            Toast.makeText(applicationContext, "OK can register", Toast.LENGTH_SHORT).show()
-            mService?.register(packageName)
+
+        if (isBound) {
+            Toast.makeText(this, ("Call").toString(), Toast.LENGTH_SHORT).show()
+
+            try {
+                mService?.register(packageName)
+
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
         }
     }
 
-    private fun handleStore() {}
-    private fun handleLoad() {}
+    private fun handleStore() {
+        val dataValue = binding.inputTextEdit.text.toString()
+        val dataType = binding.spinnerType.selectedItem.toString()
+        if (isBound) {
+            try {
+                mService?.store(packageName, dataValue, dataType)
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun handleLoad() {
+        val dataType = binding.spinnerType.selectedItem.toString()
+        if (isBound) {
+            try {
+                val dataValue = mService?.load(packageName, dataType)
+                binding.loadDataEdt.setText(dataValue)
+
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+
+        }
+
+    }
+
+
 }
